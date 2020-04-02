f0b3ee90fa5004029f9e32c595c2612511d2cdee
rosserto <62928820+rosserto@users.noreply.github.com>
2020-04-02 08:43:39 +0300

    Update yandexbot.js

--- yandexbot.js
+++ yandexbot.js
@@ -1,45 +1,46 @@
 // ==UserScript==
-// @name         New0104
+// @name         New Userscript
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        https://www.yandex.ru/*
+// @match        https://yandex.ru/*
 // @match        https://xn----7sbab5aqcbiddtdj1e1g.xn--p1ai/*
 // @grant        none
 // ==/UserScript==
 
-let googleInput = document.getElementsByName('text')[0];
-let yndx=document.getElementsByClassName("button__text")[1];
-let searchWord = 'Гобой';
-let i = 0;
-let links = document.links;
+ let yandexInput = document.getElementsByName('text')[0];
+ let button = document.getElementsByClassName('button')[1];
+ let searchWord = 'Гобой';
+ let i =0;
+ let links = document.links;
 
-if (yndx!=undefined){
-    let timerId = setInterval(()=>{
-         yandexInput.value+=searchWord[i];
-        i++;
-        if (i==searchWord.length) {
-            clearInterval(timerId);
-              yndx.click();}
-    },500);
-}
-else if (location.hostname == "yandex.ru"){
-    for (let i=0; i<links.length; i++){
-        if (links[i].href.indexOf('xn----7sbab5aqcbiddtdj1e1g.xn--p1ai')!=-1){
-            links[i].click();
-            break;
+ if ( button != undefined){
+
+ let timerId = setInterval(()=>{
+     yandexInput.value += searchWord[i];
+     i++;
+     if (i==searchWord.length) {
+         clearInterval(timerId);
+         button.click();}
+   },500);
+  }
+ else if (location.hostname == "yandex.ru"){
+  for (let i=0; i<links.length; i++){
+      if(links[i].href.indexOf('xn----7sbab5aqcbiddtdj1e1g.xn--p1ai')!=-1){
+        links[i].click();
+          break;
         }
-    }
+   }
 }
 else {
-    setInterval(()=>{
-        if (getRandom(0,100)<30) location.href = "yandex.ru/";
-        let index = getRandom(0, links.length)
+        setInterval(()=>{
+        if (getRandom(0,100)<30) Location.hostname = "yandex.ru/";
+        let index = getRandom(0,links.length);
         links[index].click();
-    },5000);
+      },5000);
 }
 
-function getRandom(min, max){
+function getRandom(min,max){
     return Math.floor(Math.random()*(max-min)+min);
 }
